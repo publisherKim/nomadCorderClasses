253c531e3a225770f5d79dff579e237e5acb3c08
es-jukim <es-jukim@smilegate.com>
2018-11-27 09:58:43 +0900

    creating a Block

--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,21 +1,29 @@
-class Human {
-  public name: string;
-  public age: number;
-  public gender: string;
+class Block {
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
 
-  constructor(name: string, age: number, gender: string) {
-    this.name = name;
-    this.age = age;
-    this.gender = gender;
+  constructor(
+    index: number,
+    hash: string,
+    previousHash: string,
+    data: string,
+    timestamp: number
+  ) {
+    this.index = index;
+    this.hash = hash;
+    this.previousHash = previousHash;
+    this.data = data;
+    this.timestamp = timestamp;
   }
 }
 
-const lynn = new Human("Lynn", 17, "female");
-console.log('instance type: ', typeof lynn, 'instance spec: ', lynn);
-const sayHi = (person: Human): string => {
-  return `Hello ${person.name}, you are ${person.age}, you are a ${person.gender}`;
-};
+const genisisBlock:Block = new Block(0, "202020202020", "", "Hello", 123456);
 
-console.log(sayHi(lynn));
+let blockchain: [Block] = [genisisBlock];
+
+console.log(blockchain);
 
 export {};
